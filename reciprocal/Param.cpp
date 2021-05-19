97d7b115b30675b2f2825e0e3fa4a62ae433b5cf
Charlie Huguenard <charlie.huguenard@gmail.com>
2021-05-19 18:40:09 -0500

    set up params (broken test)

--- reciprocal/Param.cpp
+++ reciprocal/Param.cpp
@@ -1,1 +1,33 @@
 #include "Param.h"
+#include <algorithm>
+#include <cmath>
+
+dc::Param::Param(float min, float max, float def, float step) : min(min), max(max), def(def), step(step) {
+  _val = def;
+}
+
+dc::Status dc::Param::set(float value) {
+  _val = snap(clamp(value, min, max), step);
+  return dc::Status::Ok;
+}
+
+dc::Status dc::Param::get(float &value) {
+  value = _val;
+  return dc::Status::Ok;
+}
+
+float dc::Param::snap(float value, float step) {
+  if (step > 0.0f)
+  {
+    return step * std::floor(value / step + 0.5f);
+  }
+  return value;
+}
+
+float dc::Param::clamp(float value, float min, float max) {
+  if (max > min)
+  {
+    return std::min(max, std::max(min, value));
+  }
+  return std::max(max, std::min(min, value));
+}
--- reciprocal/Param.h
+++ reciprocal/Param.h
@@ -1,7 +1,24 @@
 #pragma once
 
+#include "Status.h"
+
 namespace dc {
 class Param {
+public:
+  Param(float min, float max, float def, float step);
 
+  static float snap(float value, float step);
+  static float clamp(float value, float min, float max);
+
+  Status set(float value);
+  Status get(float& value);
+
+  const float min;
+  const float max;
+  const float def;
+  const float step;
+
+private:
+  float _val;
 };
 }
