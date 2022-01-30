e0049fbaa0702c777eab587c55ab9ba12677c8b5
Charlie Huguenard <charliehuge@users.noreply.github.com>
2022-01-30 17:17:26 -0500

    add benchmark for TrashMan (#26)

---
+++ core/benchmark/benchmark_TrashMan.cpp
@@ -0,0 +1,22 @@
+#include <benchmark/benchmark.h>
+#include "../TrashMan.h"
+
+using namespace dc;
+
+template<class T, size_t capacity>
+static void TrashMan_FillEmpty(benchmark::State& state) {
+  for (auto _ : state) {
+    TrashMan<T> trashMan(capacity);
+    // fill the trash
+    for (size_t i = 0; i < capacity; ++i) {
+      T* thing = new T();
+      if (trashMan.trash(thing) != Status::Ok) {
+        state.SkipWithError("Failed to trash a thing.");
+        return;
+      }
+    }
+    // The destructor will block until empty
+  }
+}
+
+BENCHMARK(TrashMan_FillEmpty<int, 512>);
