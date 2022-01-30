6133983e6b1d29bd8527211d006b49eea8ef81b8
Charlie Huguenard <charliehuge@users.noreply.github.com>
2022-01-30 09:13:25 -0500

    Benchmarks (#23)
    * add google benchmark
    
    * compare SPSCQ and MPMCQ for fun
    
    * do not build/test google benchmark, they did that already
    
    * add benchmark workflow
    
    * explicitly build test targets to keep build times down
    
    * store benchmark result and alert on regression
    
    * fix typo
    
    * fix typo
    
    * maybe fix writing json
    
    * fix paths
    
    * benchmark_out
    
    * publish to gh pages
    
    * no deploy permission

---
+++ core/benchmark/benchmark_SPSCQ.cpp
@@ -0,0 +1,55 @@
+#include <benchmark/benchmark.h>
+#include "../../third_party/rigtorp/MPMCQueue.h"
+#include "../SPSCQ.h"
+
+using namespace dc;
+
+template<class T, size_t capacity>
+static void SPSCQ_PushPop(benchmark::State& state) {
+  SPSCQ<T> q(capacity);
+
+  for (auto _: state) {
+    q.push([](T&) {
+      return Status::Ok;
+    });
+    q.pop([](T&) {
+      return Status::Ok;
+    });
+  }
+}
+
+BENCHMARK(SPSCQ_PushPop<int, 64>);
+BENCHMARK(SPSCQ_PushPop<double, 512>);
+BENCHMARK(SPSCQ_PushPop<float, 128>);
+
+template<class T, size_t capacity>
+static void SPSCQ_FillDrain(benchmark::State& state) {
+  SPSCQ<T> q(capacity);
+
+  for (auto _: state) {
+    while (q.push([](T&) { return Status::Ok; }) == Status::Ok);
+    while (q.pop([](T&) { return Status::Ok; }) == Status::Ok);
+  }
+}
+
+BENCHMARK(SPSCQ_FillDrain<int, 64>);
+BENCHMARK(SPSCQ_FillDrain<double, 512>);
+BENCHMARK(SPSCQ_FillDrain<float, 128>);
+
+
+template<class T, size_t capacity>
+static void MPMCQ_FillDrain(benchmark::State& state) {
+  rigtorp::MPMCQueue<T> q(capacity);
+
+  for (auto _: state) {
+    while (q.try_push(T()));
+    T t;
+    while (q.try_pop(t));
+  }
+}
+
+BENCHMARK(MPMCQ_FillDrain<int, 64>);
+BENCHMARK(MPMCQ_FillDrain<double, 512>);
+BENCHMARK(MPMCQ_FillDrain<float, 128>);
+
+BENCHMARK_MAIN();
