981ae2fadd3e391db492d13e537ea11f37b00618
Charlie Huguenard <charlie.huguenard@gmail.com>
2022-01-29 19:02:29 -0500

    compare SPSCQ and MPMCQ for fun

--- core/benchmark/benchmark_SPSCQ.cpp
+++ core/benchmark/benchmark_SPSCQ.cpp
@@ -1,13 +1,14 @@
 #include <benchmark/benchmark.h>
+#include "../../third_party/rigtorp/MPMCQueue.h"
 #include "../SPSCQ.h"
 
 using namespace dc;
 
 template<class T, size_t capacity>
-static void BM_PushPop(benchmark::State& state) {
+static void SPSCQ_PushPop(benchmark::State& state) {
   SPSCQ<T> q(capacity);
 
-  for (auto _ : state) {
+  for (auto _: state) {
     q.push([](T&) {
       return Status::Ok;
     });
@@ -16,25 +17,39 @@ static void BM_PushPop(benchmark::State& state) {
     });
   }
 }
-BENCHMARK(BM_PushPop<int, 64>);
-BENCHMARK(BM_PushPop<double, 512>);
-BENCHMARK(BM_PushPop<float, 128>);
+
+BENCHMARK(SPSCQ_PushPop<int, 64>);
+BENCHMARK(SPSCQ_PushPop<double, 512>);
+BENCHMARK(SPSCQ_PushPop<float, 128>);
 
 template<class T, size_t capacity>
-static void BM_FillDrain(benchmark::State& state) {
+static void SPSCQ_FillDrain(benchmark::State& state) {
   SPSCQ<T> q(capacity);
 
-  for (auto _ : state) {
-    while (q.size() < capacity) {
-      q.push([](T&) { return Status::Ok; });
-    }
-    while (q.size() > 0) {
-      q.pop([](T&) { return Status::Ok; });
-    }
+  for (auto _: state) {
+    while (q.push([](T&) { return Status::Ok; }) == Status::Ok);
+    while (q.pop([](T&) { return Status::Ok; }) == Status::Ok);
   }
 }
-BENCHMARK(BM_FillDrain<int, 64>);
-BENCHMARK(BM_FillDrain<double, 512>);
-BENCHMARK(BM_FillDrain<float, 128>);
+
+BENCHMARK(SPSCQ_FillDrain<int, 64>);
+BENCHMARK(SPSCQ_FillDrain<double, 512>);
+BENCHMARK(SPSCQ_FillDrain<float, 128>);
+
+
+template<class T, size_t capacity>
+static void MPMCQ_FillDrain(benchmark::State& state) {
+  rigtorp::MPMCQueue<T> q(capacity);
+
+  for (auto _: state) {
+    while (q.try_push(T()));
+    T t;
+    while (q.try_pop(t));
+  }
+}
+
+BENCHMARK(MPMCQ_FillDrain<int, 64>);
+BENCHMARK(MPMCQ_FillDrain<double, 512>);
+BENCHMARK(MPMCQ_FillDrain<float, 128>);
 
 BENCHMARK_MAIN();
