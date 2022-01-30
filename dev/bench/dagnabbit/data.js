9afad5143296cf9b4f25ed74d48c9e5fce06866c
github-action-benchmark <github@users.noreply.github.com>
2022-01-30 18:02:27 +0000

    add dagnabbit Benchmark (googlecpp) benchmark result for 70ba8de90a1e2bfcf7165eef5b05fee3b4aa976a

---
+++ dev/bench/dagnabbit/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1643565747350,
+  "repoUrl": "https://github.com/derelictcomputer/reciprocal",
+  "entries": {
+    "dagnabbit Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "charlie.huguenard@gmail.com",
+            "name": "Charlie Huguenard",
+            "username": "charliehuge"
+          },
+          "committer": {
+            "email": "charlie.huguenard@gmail.com",
+            "name": "Charlie Huguenard",
+            "username": "charliehuge"
+          },
+          "distinct": true,
+          "id": "70ba8de90a1e2bfcf7165eef5b05fee3b4aa976a",
+          "message": "add dagnabbit benchmarks to workflow",
+          "timestamp": "2022-01-30T13:01:25-05:00",
+          "tree_id": "522a6e45aed5dfa726748cf39b67f795b52d813f",
+          "url": "https://github.com/derelictcomputer/reciprocal/commit/70ba8de90a1e2bfcf7165eef5b05fee3b4aa976a"
+        },
+        "date": 1643565745871,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "Node_Passthrough_ConnectDisconnect<double, double, 16>",
+            "value": 340.9375484150413,
+            "unit": "ns/iter",
+            "extra": "iterations: 2031910\ncpu: 340.92174358116256 ns\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
