6a81f49018c9a753cbb93693de23e4a9d6054b47
Charlie Huguenard <charlie.huguenard@gmail.com>
2022-01-30 12:21:35 -0500

    use common PassthroughNode

--- dagnabbit/test/test_Graph.cpp
+++ dagnabbit/test/test_Graph.cpp
@@ -1,49 +1,9 @@
 #include <chrono>
 #include <gtest/gtest.h>
 #include "../Graph.h"
-#include "../Message.h"
+#include "../dev_helpers/PassthroughNode.h"
 
 using namespace dc;
-
-template<class DataType, class TimeType>
-class PassthroughNode : public Node<TimeType> {
-public:
-  using MessageType = Message<DataType, TimeType>;
-  using InputPortType = InputPort<MessageType>;
-  using OutputPortType = OutputPort<MessageType>;
-
-  PassthroughNode(size_t queueSize, size_t maxOutputConnections) :
-      _input("in", queueSize),
-      _output("out", maxOutputConnections) {
-    // input
-    this->_inputs.push_back(&_input);
-    // output
-    this->_outputs.push_back(&_output);
-  }
-
-  Status process(const TimeType&, const TimeType&) override {
-    MessageType msg;
-    while (popMessage(msg) == Status::Ok) {
-      const auto status = _output.pushToConnections(msg);
-      if (status != Status::Ok) {
-        return status;
-      }
-    }
-    return Status::Ok;
-  }
-
-  Status pushMessage(const MessageType& msg) {
-    return _input.pushMessage(msg);
-  }
-
-  Status popMessage(MessageType& msg) {
-    return _input.popMessage(msg);
-  }
-
-private:
-  InputPortType _input;
-  OutputPortType _output;
-};
 
 TEST(Graph, AddRemove) {
   using TimeType = double;
